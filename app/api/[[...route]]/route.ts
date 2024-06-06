9976e272ce5a55380d6b3a596095985965ddab9c
dkiriinya <dkiriinya06sc@gmail.com>
2024-06-06 23:54:44 +0300

    added comonents for the header

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,16 +1,12 @@
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
-import authors from './authors'
-import books from './books'
-
-
 export const runtime = 'edge';
 
 const app = new Hono().basePath('/api')
 
-const routes = app.route('/authors', authors).route('/books', books);
+app.get("hello",(c) => {
+    return c.json({message:"hello world"})
+})
 
-
-export type AppType = typeof routes;
 export const GET = handle(app)
 export const POST = handle(app)
