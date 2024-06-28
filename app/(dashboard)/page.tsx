8834c85141935778232d162228454a7ec52197fb
dkiriinya <dkiriinya06sc@gmail.com>
2024-06-28 19:32:22 +0300

    added data grid that consists of data tables

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,15 +1,11 @@
 "use client"
 
-import { Button } from "@/components/ui/button";
-import { useNewAccount } from "@/features/accounts/hooks/use-new-accounts";
+import { DataGrid } from "@/components/data-grid";
 
-export default function Home() {
-  const {onOpen} = useNewAccount();
+export default function DashboardPage() {
   return(
-    <div>
-     <Button onClick={onOpen}>
-      Add an Account
-     </Button>
+    <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+      <DataGrid />
     </div>
     
   )
