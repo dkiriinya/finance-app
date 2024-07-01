f7c3bcb1f37c7728f820cf4a6379c5383ff7472f
dkiriinya <dkiriinya06sc@gmail.com>
2024-07-01 23:06:07 +0300

    fixed error

--- app/(dashboard)/transactions/import-table.tsx
+++ app/(dashboard)/transactions/import-table.tsx
@@ -5,15 +5,14 @@ import {
     TableHead,
     TableRow,
     TableHeader,
-
 } from "@/components/ui/table";
 import { TableHeadSelect } from "./table-head-select"
 
 type Props = {
     headers: string[];
     body: string[][];
-    selectedColumns: Record<string,string | null>;
-    onTableHeadSelectChange: (columnIndex:number,value:string | null) => void;
+    selectedColumns: Record<string, string | null>;
+    onTableHeadSelectChange: (columnIndex: number, value: string | null) => void;
 }
 
 export const ImportTable = ({
@@ -25,33 +24,31 @@ export const ImportTable = ({
     return (
         <div className="rounded-md border overflow-hidden">
             <Table>
-                <TableHeader className="bg-muted">
+                <TableHeader className="bg-muted" key={'tablehead'}>
                     <TableRow>
-                        {headers.map((_item, index) => (
-                           <TableHead>
-                                <TableHeadSelect 
+                        {headers.map((item, index) => (
+                            <TableHead key={index}>
+                                <TableHeadSelect
                                     columnIndex={index}
                                     selectedColumns={selectedColumns}
                                     onChange={onTableHeadSelectChange}
                                 />
-                           </TableHead>
+                            </TableHead>
                         ))}
                     </TableRow>
                 </TableHeader>
                 <TableBody>
-                    {body.map((row:string[], index) => (
-                        <TableRow key={index}>
-                            {row.map((cell, index) => (
-                                <TableCell key={index}>
+                    {body.map((row: string[], rowIndex) => (
+                        <TableRow key={rowIndex}>
+                            {row.map((cell, cellIndex) => (
+                                <TableCell key={cellIndex}>
                                     {cell}
                                 </TableCell>
                             ))}
-
                         </TableRow>
                     ))}
                 </TableBody>
             </Table>
         </div>
-        
     );
 };
